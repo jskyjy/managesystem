5768bcde27cbd2194a5383f1bbbda0752cb8950a
unknown <1741752618@qq.com>
2019-09-23 13:56:52 +0800

    新建登录模块

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+//  @ 是webpack封装好的，就代表了src文件夹
+import Login from '@/components/login/login.vue'
 
 Vue.use(Router)
 
@@ -7,6 +9,11 @@ export default new Router({
   routes: [
     {
       path: '/'
+    },
+    {
+      name:'login',
+      path:'/login',
+      component:Login
     }
   ]
 })
